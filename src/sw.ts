066d5cc23cc6d557acbc74a351a109ce57b03aab
Myrotvorets <support@myrotvorets.center>
2021-01-30 05:52:42 +0200

    Update service worker

--- src/sw.ts
+++ src/sw.ts
@@ -2,19 +2,24 @@
 
 import { cacheNames, clientsClaim, skipWaiting } from 'workbox-core';
 import { registerRoute, setCatchHandler } from 'workbox-routing';
-import { getCacheKeyForURL, precacheAndRoute } from 'workbox-precaching';
+import { cleanupOutdatedCaches, matchPrecache, precacheAndRoute } from 'workbox-precaching';
 import { NetworkFirst, StaleWhileRevalidate } from 'workbox-strategies';
 import { CacheableResponsePlugin } from 'workbox-cacheable-response';
 import { ExpirationPlugin } from 'workbox-expiration';
 
 skipWaiting();
 clientsClaim();
+cleanupOutdatedCaches();
+
+declare let self: ServiceWorkerGlobalScope;
+const manifest = self.__WB_MANIFEST;
+precacheAndRoute(manifest, { ignoreURLParametersMatching: [/.*/u] });
 
 registerRoute(
     ({ request }) => request.mode === 'navigate',
     new NetworkFirst({
         cacheName: cacheNames.precache,
-        networkTimeoutSeconds: 5,
+        networkTimeoutSeconds: 3,
         matchOptions: {
             ignoreSearch: true,
         },
@@ -44,17 +49,11 @@ registerRoute(
     }),
 );
 
-declare let self: ServiceWorkerGlobalScope;
-const manifest = self.__WB_MANIFEST;
-precacheAndRoute(manifest, {});
-
-setCatchHandler(({ request }) => {
-    if (typeof request !== 'string' && request.mode === 'navigate') {
-        const key = getCacheKeyForURL('index.html');
-        if (key) {
-            return caches.match(key) as Promise<Response>;
-        }
+setCatchHandler(async (options) => {
+    const { destination } = options.request;
+    if (destination === 'document') {
+        return (await matchPrecache('index.html')) || Response.error();
     }
 
-    return Promise.reject(Response.error());
+    return Response.error();
 });
